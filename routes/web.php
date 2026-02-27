1618f6f51c9cd836fb8ea6cc76860889e773c7e5
Fata-Haidar <fatahaidaraly@gmail.com>
2026-02-27 21:28:10 +0700

    Praktikum

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
  'create', 'store', 'update', 'destroy'
 ]);
+//Route::get('/greeting', function () {
+//return view('blog.hello', ['name' => 'Andi']);
+//});
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
